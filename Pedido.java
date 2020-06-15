7235ec5fc5e9129d24abe9d3c5705fdd30d9fdcb
RomanoFelix16 <43682358+RomanoFelix16@users.noreply.github.com>
2020-06-15 18:32:50 -0300

    Agregado precio total

---
+++ Pedido.java
@@ -0,0 +1,101 @@
+package com.unla.grupo1oo22020.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="pedido")
+public class Pedido {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idPedido;
+	
+	@Column(name="fechaPedido")
+	private LocalDate fechaPedido;
+	
+	@OneToOne(cascade=CascadeType.MERGE)
+	private Producto producto;
+	
+	@Column(name="cantidad")
+	private int cantidad;
+	
+	@OneToOne(cascade=CascadeType.MERGE)
+	private Local local;
+	
+	@Column(name="precioTotal")
+	private float precioTotal;
+
+	public Pedido() {
+		
+	}
+
+	public Pedido(long idPedido, LocalDate fechaPedido, Producto producto, int cantidad, Local local, float precioTotal) {
+		super();
+		this.setIdPedido(idPedido);
+		this.fechaPedido = fechaPedido;
+		this.producto = producto;
+		this.cantidad = cantidad;
+		this.local = local;
+		this.precioTotal = precioTotal;
+	}
+
+	public long getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(long idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public LocalDate getFechaPedido() {
+		return fechaPedido;
+	}
+
+	public void setFechaPedido(LocalDate fechaPedido) {
+		this.fechaPedido = fechaPedido;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public Local getLocal() {
+		return local;
+	}
+
+	public void setLocal(Local local) {
+		this.local = local;
+	}
+
+	public float getPrecioTotal() {
+		return precioTotal;
+	}
+	
+	public void setPrecioTotal(float precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+	
+	public float precioTotal(Producto producto, int cantidad) {
+		return precioTotal = producto.getPrecioUnitario() * cantidad;
+	}
+}
